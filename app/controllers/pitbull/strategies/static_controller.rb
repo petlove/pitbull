bc4f0a7e5c42c621d49d1b6aa3952474c9960482
Marcelo Toledo <marcelotoledo5000@gmail.com>
2019-12-13 18:03:49 -0300

    Remove engine

--- app/controllers/pitbull/strategies/static_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Pitbull
-  module Strategies
-    class StaticController < ApplicationController
-      include Pitbull::Strategies::Static
-    end
-  end
-end
--- lib/pitbull/engine.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails'
-
-module Pitbull
-  class Engine < Rails::Engine
-  end
-end
